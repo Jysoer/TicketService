8687619cb1cebc0c83522fe474ca56d2dbd5ce5f
Jysoer <Onisavi@gmail.com>
2024-10-10 21:02:25 +0200

    - Fixes in main according to the code

--- SecondTask/src/Service/TicketService.java
+++ SecondTask/src/Service/TicketService.java
@@ -1,12 +1,15 @@
 package Service;
 
+import Enums.StadiumSector;
 import Model.Ticket;
+
+import java.time.LocalDateTime;
 
 public class TicketService {
     public static void main(String[] args) {
-        Ticket ticket1 = new Ticket(1L, "Bohemy", 123, 111111, true, 'A', 32);
+        Ticket ticket1 = new Ticket(1L, "Bohemy", 123, LocalDateTime.now(), true, StadiumSector.A, 32);
         System.out.println("Creation of ticket №1 took " + ticket1.getDuration() + " ms. Price: " + ticket1.getTicketPrice() + ".");
-        Ticket ticket2 = new Ticket("Bohemy", 123, 111111);
+        Ticket ticket2 = new Ticket("Bohemy", 123, LocalDateTime.now());
         System.out.println("Creation of ticket №2 took " + ticket2.getDuration() + " ms. Price: " + ticket2.getTicketPrice() + ".");
         Ticket ticket3 = new Ticket();
         System.out.println("Creation of ticket №3 took " + ticket3.getDuration() + " ms. Price: " + ticket3.getTicketPrice() + ".");
